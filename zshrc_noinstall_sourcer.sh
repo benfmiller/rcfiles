6021cde0cd600be1836b1ee812d80a4c94c252ff
Ben Miller <ben.f.miller24@gmail.com>
2021-01-23 23:02:56 -0700

    added theme to sourcer

--- zshrc_noinstall_sourcer.sh
+++ zshrc_noinstall_sourcer.sh
@@ -1,4 +1,19 @@
 source ~/rcfiles/zsh_zsh.sh
+# Theme{{
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="linuxonly"
+#fishy
+#obraun
+
+# Set list of themes to pick from when loading at random
+# Setting this variable when ZSH_THEME=random will cause zsh to load
+# a theme from this variable instead of looking in $ZSH/themes/
+# If set to an empty array, this variable will have no effect.
+# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+#}}
 # Plugins{{
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
