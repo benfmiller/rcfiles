9dd576b64f56c873403a1ab89b86f63f0130f168
Ben Miller <ben.f.miller24@gmail.com>
2021-03-12 21:15:52 -0700

    added pullrcs to tupdate

--- zsh/zsh_user.sh
+++ zsh/zsh_user.sh
@@ -19,7 +19,7 @@ cpnviminit () {
 }
 #}}
 #System{{
-alias tupdate="sudo apt update && sudo apt upgrade -y"
+alias tupdate="sudo apt update && sudo apt upgrade -y && pullrcs"
 alias sinstal="sudo apt install"
 #}}
 #Tmux{{
