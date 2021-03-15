319088b55204cb376534ff7664acd110161f100d
Ben Miller <ben.f.miller24@gmail.com>
2021-03-15 17:06:11 -0600

    updated tupdate

--- zsh/zsh_user.sh
+++ zsh/zsh_user.sh
@@ -19,7 +19,7 @@ cpnviminit () {
 }
 #}}
 #System{{
-alias tupdate="sudo apt update && sudo apt upgrade -y ; echo ; echo "PullingRcs" ; pullrcs"
+alias tupdate="sudo apt update && sudo apt upgrade -y ; echo ; echo \"PullingRcs\" ; pullrcs ; zsh"
 alias sinstal="sudo apt install"
 #}}
 #Tmux{{
