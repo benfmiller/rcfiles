ce12b709177e655abf391e172c8385152c9816ca
Ben Miller <benfmiller132@gmail.com>
2021-07-04 13:57:31 -0600

    added brackets to time in winbash

--- zsh/winbash.sh
+++ zsh/winbash.sh
@@ -68,7 +68,7 @@ then
 else
 	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
 	# PS1="$PS1"'\n'                 # new line
-	PS1="$PS1"'\[\033[94m\]\t '     # time
+	PS1="$PS1"'\[\033[94m\][\t] '     # time
 	PS1="$PS1"'\[\033[32m\]'       # change to green
 	PS1="$PS1"'\u@\h '             # user@host<space>
 	PS1="$PS1"'\[\033[35m\]'       # change to purple
