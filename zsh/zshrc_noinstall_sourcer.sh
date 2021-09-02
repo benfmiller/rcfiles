75f8d1dc02164cbe4f55bac1a0785060b2062c0a
Ben Miller <benfmiller132@gmail.com>
2021-09-02 11:31:33 -0600

    updated sourcers for current config

--- zsh/zshrc_noinstall_sourcer.sh
+++ zsh/zshrc_noinstall_sourcer.sh
@@ -1,26 +1,5 @@
 source ~/rcfiles/zsh/zsh_zsh.sh
-# wsl Moving Aliases {{
-alias cdp="cd /mnt/c/users/benfm/GoogleDrive/CProjects"
-alias cn="cd /mnt/c/users/benfm/GoogleDrive/CProjects/Cnotes"
-alias cw="cdp && cd ClassWork"
-alias ch="cd /mnt/c/users/benfm/"
-export CH="/mnt/c/users/benfm/"
-#}}
-# Theme{{
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time oh-my-zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="linuxonly"
-#fishy
-#obraun
-
-# Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in $ZSH/themes/
-# If set to an empty array, this variable will have no effect.
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
-#}}
+source ~/rcfiles/zsh/zsh_user.sh
 # Plugins{{
 # Which plugins would you like to load?
 # Standard plugins can be found in $ZSH/plugins/
@@ -30,7 +9,6 @@ ZSH_THEME="linuxonly"
 plugins=(git python vi-mode tmux jump z)
 #}}
 source $ZSH/oh-my-zsh.sh
-source ~/rcfiles/zsh/zsh_user.sh
 
 export DISPLAY="`grep nameserver /etc/resolv.conf | sed 's/nameserver //'`:0"
 
--- zsh/zshrc_sourcer.sh
+++ zsh/zshrc_sourcer.sh
@@ -1,37 +1,6 @@
 source ~/rcfiles/zsh/zsh_zsh.sh
-# wsl Moving Aliases {{
-alias cdp="cd /mnt/c/users/benfm/GoogleDrive/CProjects"
-alias cn="cd /mnt/c/users/benfm/GoogleDrive/CProjects/Cnotes"
-alias cw="cdp && cd ClassWork"
-alias ch="cd /mnt/c/users/benfm/"
-export CH="/mnt/c/users/benfm/"
-#}}
-# Theme{{
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time oh-my-zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="linuxonly"
-#fishy
-#obraun
-
-# Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in $ZSH/themes/
-# If set to an empty array, this variable will have no effect.
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
-#}}
-# Plugins{{
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git python vi-mode tmux sudo zsh-interactive-cd jump fzf)
-# jump mark unmark marks
-#}}
-source $ZSH/oh-my-zsh.sh
 source ~/rcfiles/zsh/zsh_user.sh
+source $ZSH/oh-my-zsh.sh
 
 export DISPLAY="`grep nameserver /etc/resolv.conf | sed 's/nameserver //'`:0"
 
