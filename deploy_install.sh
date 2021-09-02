65978ea6dc26ddb38ff0db15e96959e6056711d2
Ben Miller <benfmiller132@gmail.com>
2021-09-02 11:31:01 -0600

    updated deploys to be more useful

--- deploy_install.sh
+++ deploy_install.sh
@@ -7,51 +7,34 @@ if [ `whoami` = 'root' ]
   then
     echo "Don't use root!"
     return 1
-    echo "Installing fzf and ripgrep"
-    sudo apt install fzf ripgrep
-
-    echo "Moving old config files to $ORG_HOME/.config/old_config"
-    mkdir $ORG_HOME/.config/old_config
-    if test -f $ORG_HOME/.vimrc; then
-        mv $ORG_HOME/.vimrc $ORG_HOME/.config/old_config/.vimrc
-    fi
-    if test -f $ORG_HOME/.zshrc; then
-        mv $ORG_HOME/.zshrc $ORG_HOME/.config/old_config/.zshrc
-    fi
-    if test -f $ORG_HOME/.tmux.conf; then
-        mv $ORG_HOME/.tmux.conf $ORG_HOME/.config/old_config/.tmux.conf
-    fi
-
-    echo "copying config"
-    cp $ORG_HOME/rcfiles/tmux/tmux_sourcer.conf $ORG_HOME/.tmux.conf
-    cp $ORG_HOME/rcfiles/vim/vimrc_sourcer.vim $ORG_HOME/.vimrc
-    cp $ORG_HOME/rcfiles/zsh/zshrc_sourcer.sh $ORG_HOME/.zshrc
-
-    tmux source-file $ORG_HOME/.tmux.conf
-    echo "All Good!"
-    exit
-    zsh
-else
-    # echo "If ran as root, also installs fzf and ripgrep"
-    echo "Moving old config files to ~/.config/old_config"
-    mkdir ~/.config/old_config
-    if test -f ~/.vimrc; then
-        mv ~/.vimrc ~/.config/old_config/.vimrc
-    fi
-    if test -f ~/.zshrc; then
-        mv ~/.zshrc ~/.config/old_config/.zshrc
-    fi
-    if test -f ~/.tmux.conf; then
-        mv ~/.tmux.conf ~/.config/old_config/.tmux.conf
-    fi
-
-    echo "copying config"
-    cp ~/rcfiles/tmux/tmux_sourcer.conf ~/.tmux.conf
-    cp ~/rcfiles/tmux/tmux_plugins_sourcer.conf ~/.tmux_plugins.conf
-    cp ~/rcfiles/vim/vimrc_sourcer.vim ~/.vimrc
-    cp ~/rcfiles/zsh/zshrc_sourcer.sh ~/.zshrc
-
-    tmux source-file ~/.tmux.conf
-    echo "All Good!"
-    zsh
 fi
+
+echo "Moving old config files to ~/.config/old_config"
+mkdir -p ~/.config/old_config
+if test -f ~/.vimrc; then
+    mv ~/.vimrc ~/.config/old_config/.vimrc
+fi
+if test -f ~/.zshrc; then
+    mv ~/.zshrc ~/.config/old_config/.zshrc
+fi
+if test -f ~/.tmux.conf; then
+    mv ~/.tmux.conf ~/.config/old_config/.tmux.conf
+fi
+
+echo "copying config"
+cp ~/rcfiles/tmux/tmux_sourcer.conf ~/.tmux.conf
+cp ~/rcfiles/tmux/tmux_plugins_sourcer.conf ~/.tmux_plugins.conf
+cp ~/rcfiles/vim/vimrc_sourcer.vim ~/.vimrc
+cp ~/rcfiles/zsh/zshrc_sourcer.sh ~/.zshrc
+
+tmux source-file ~/.tmux.conf
+echo "All Good!"
+echo "errors are normal"
+echo "also install fzf, bat, ripgrep, neovim"
+echo "run install_ tmux zsh and plug"
+echo "prefix I in tmux to install"
+echo "print_ is your friend"
+echo "run cpnviminit if using neovim"
+echo "configure your ssh"
+echo "also maybe install pandoc and texlive stuff"
+zsh
--- deploy_no_install.sh
+++ deploy_no_install.sh
@@ -4,26 +4,32 @@ if [ `whoami` = 'root' ]
   then
     echo "Don't use as root!"
     return 1
-else
-    echo "Moving old config files to ~/.config/old_config"
-    mkdir ~/.config/old_config
-    if test -f "~/.vimrc"; then
-        mv ~/.vimrc ~/.config/old_config/.vimrc
-    fi
-    if test -f "~/.zshrc"; then
-        mv ~/.zshrc ~/.config/old_config/.zshrc
-    fi
-    if test -f "~/.tmux.conf"; then
-        mv ~/.tmux.conf ~/.config/old_config/.tmux_conf
-    fi
-
-    echo "copying config"
-    cp ~/rcfiles/tmux/tmux_sourcer.conf ~/.tmux.conf
-    cp ~/rcfiles/tmux/tmux_plugins_sourcer.conf ~/.tmux_plugins.conf
-    cp ~/rcfiles/vim/vimrc_noinstall_sourcer.vim ~/.vimrc
-    cp ~/rcfiles/zsh/zshrc_noinstall_sourcer.sh ~/.zshrc
 fi
+
+echo "Moving old config files to ~/.config/old_config"
+mkdir -p ~/.config/old_config
+if test -f "~/.vimrc"; then
+    mv ~/.vimrc ~/.config/old_config/.vimrc
+fi
+if test -f "~/.zshrc"; then
+    mv ~/.zshrc ~/.config/old_config/.zshrc
+fi
+if test -f "~/.tmux.conf"; then
+    mv ~/.tmux.conf ~/.config/old_config/.tmux_conf
+fi
+
+echo "copying config"
+cp ~/rcfiles/tmux/tmux_sourcer.conf ~/.tmux.conf
+cp ~/rcfiles/tmux/tmux_plugins_sourcer.conf ~/.tmux_plugins.conf
+cp ~/rcfiles/vim/vimrc_noinstall_sourcer.vim ~/.vimrc
+cp ~/rcfiles/zsh/zshrc_noinstall_sourcer.sh ~/.zshrc
 
 tmux source-file ~/.tmux.conf
 echo "All Good!"
+echo "errors are normal"
+echo "run install_ tmux zsh and plug, doesn't require root"
+echo "prefix I in tmux to install"
+echo "print_ is your friend"
+echo "run cpnviminit if using neovim"
+echo "configure your ssh"
 zsh
