d9d6a0a1a89f8a95bef99e8f7900088cc2569680
Ben Miller <benfmiller132@gmail.com>
2021-09-07 15:54:40 -0600

    moved zsh zsh to zsh user

--- zsh/zshrc_noinstall_sourcer.sh
+++ zsh/zshrc_noinstall_sourcer.sh
@@ -1,13 +1,4 @@
-source ~/rcfiles/zsh/zsh_zsh.sh
 source ~/rcfiles/zsh/zsh_user.sh
-# Plugins{{
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git python vi-mode tmux jump z)
-#}}
 source $ZSH/oh-my-zsh.sh
 
 export DISPLAY="`grep nameserver /etc/resolv.conf | sed 's/nameserver //'`:0"
