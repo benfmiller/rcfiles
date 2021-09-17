ad57d42ea977bbb5481e838666acdb2ad452b1a8
Ben Miller <benfmiller132@gmail.com>
2021-09-17 11:45:07 -0600

    tmux_ch lets you enter arbitrary category now

--- scripts/tmux_ch.sh
+++ scripts/tmux_ch.sh
@@ -4,10 +4,13 @@ core_utils=`echo "git xargs find mv sed awk" | tr ' ' '\n'`
 
 selected=`printf "$languages\n$core_utils" | fzf`
 if [[ -z $selected ]]; then
+    read -p "Enter Topic: " selected
+fi
+if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 query=`echo $query | tr ' ' '+'`
 
-tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+tmux neww bash -c "echo \"curl cht.sh/$selected/$query\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
