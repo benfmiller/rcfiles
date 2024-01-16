77c720bf5f0124a750b8c83a5f1d11f3b4fdc5a7
Ben Miller <millrben@amazon.com>
2024-01-15 20:03:21 -0800

    ssh_tmux_attach now creates new session if doesn't exist

--- scripts/ssh_tmux_attach.sh
+++ scripts/ssh_tmux_attach.sh
@@ -4,4 +4,13 @@ session=$(tmux display-message -p "#S")
 
 echo "$session"
 
-ssh dev tmux -2 attach -d -t $session
+REMOTE_WS=$(ssh dev tmux ls)
+
+
+if [[ $(echo "$REMOTE_WS" | rg "$session") ]]; then
+    echo "Found $session on remote. Attaching"
+    ssh dev tmux -2 attach -d -t $session
+else
+    echo "Did not find $session on remote. Creating"
+    ssh dev tmux -2 new-session -s $session
+fi
